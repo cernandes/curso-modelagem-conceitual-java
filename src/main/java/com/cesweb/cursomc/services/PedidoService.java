2b7c3a715dcdfb822b606f09a0a87570dcb793e6
Claudio <cernandes.silva@gmail.com>
2020-12-14 23:42:12 -0300

    Pasta assets com diagramas UML

--- src/main/java/com/cesweb/cursomc/services/PedidoService.java
+++ src/main/java/com/cesweb/cursomc/services/PedidoService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.cesweb.cursomc.domain.Categoria;
 import com.cesweb.cursomc.domain.Pedido;
 import com.cesweb.cursomc.repositories.PedidoRepository;
 import com.cesweb.cursomc.services.exceptions.ObjectNotFoundException;
@@ -19,6 +18,6 @@ public class PedidoService {
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 }
